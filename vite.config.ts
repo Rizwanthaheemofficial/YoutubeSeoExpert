6d8a0a5541f8347761d28d5a5b03b80f0ea96e1f
Rizwan Ali Thaheem <rizwanthaheemofficial@gmail.com>
2026-01-12 19:25:00 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
 import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    base: './',
-
-    plugins: [react()],
-
-    server: {
-      port: 3000,
-      host: true
-    },
-
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.')
-      }
+export default defineConfig({
+  base: './',          // 🔥 REQUIRED FOR VERCEL
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.')
     }
   }
 })
